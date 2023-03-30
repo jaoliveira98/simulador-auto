99d070e8c05471ba3c7dbb5dd96758c9a6b7f1cb
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-30 17:00:57 +0100

    Router implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import "./App.css";
-import { Search, Highlight } from "./components/index.js";
-import { video, logo } from "./assets/index.js";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import CarDetailsPage from "./pages/CarDetailsPage";
 
 function App() {
   return (
     <>
-      <div id="video-background">
-        <video autoPlay muted loop>
-          <source src={video} type="video/webm" />
-        </video>
-      </div>
-      <Highlight />
-      <div className="flex items-center justify-center h-[100vh]">
-        <div className="flex items-center flex-col">
-          <img src={logo} alt="Logo" className="w-[300px]" />
-          <Search />
-        </div>
-      </div>
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/car-detail" element={<CarDetailsPage />} />
+      </Routes>
     </>
   );
 }
