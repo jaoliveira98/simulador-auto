988df4c3a7216458b61fa5ff5674106e68182103
Joao Oliveira <jaoliveira98@gmail.com>
2023-03-31 11:55:30 +0100

    Update CarDetails

--- src/components/Search/elements/CarDetails.jsx
+++ src/components/Search/elements/CarDetails.jsx
@@ -1,4 +1,4 @@
-function CarDetails({
+const CarDetails = ({
   heading,
   description,
   selectedCar,
@@ -7,34 +7,34 @@ function CarDetails({
   hp,
   binary,
   price,
-}) {
+}) => {
   return (
-    <div className="container mx-auto overflow-hidden bg-white shadow sm:rounded-lg">
-      <div className="px-4 py-5 sm:px-6">
-        <h3 className="text-gray-900">{heading}</h3>
-        <p className="mt-1 text-gray-500">{description}</p>
-      </div>
-      <div className="border-t border-gray-200">
-        <dl>
-          {selectedCar && (
-            <div className="bg-gray-50 text-black px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-              <p>Make: {selectedCar.make}</p>
-              <p>Model: {selectedCar.model}</p>
-              <p>Year: {year}</p>
-              <p>Engine: {engine}</p>
-              <p>Horse Power: {Math.ceil(hp)} HP</p>
-              <p>Binary: {Math.ceil(binary)} NM</p>
-              <p>Price: {Math.ceil(price)} €</p>
+    selectedCar && (
+      <>
+        <div className="container mx-auto">
+          <div className="grid grid-col-1 md:grid-cols-2 ">
+            <div>
+              <p>
+                {selectedCar.make} {selectedCar.model}
+              </p>
               <img
                 src={selectedCar.image}
                 alt={`${selectedCar.make} ${selectedCar.model}`}
                 style={{ maxWidth: "300px" }}
               />
             </div>
-          )}
-        </dl>
-      </div>
-    </div>
+
+            <div className="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <p>Year: {year}</p>
+              <p>Engine: {engine}</p>
+              <p>Horse Power: {Math.ceil(hp)} HP</p>
+              <p>Binary: {Math.ceil(binary)} NM</p>
+              <p>Price: {Math.ceil(price)} €</p>
+            </div>
+          </div>
+        </div>
+      </>
+    )
   );
-}
+};
 export default CarDetails;
